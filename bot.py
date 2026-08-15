160f7a76240a126ecb6a2923e59a7af4802007c0
GEN10sMC <skpminecraft0@gmail.com>
2026-08-15 23:12:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import asyncio
 import logging
 import os
 import re
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from threading import Thread
 from aiogram import Bot, Dispatcher, F, types
 
 TOKEN = os.getenv("TOKEN")
@@ -36,6 +38,20 @@ BANNED_WORDS = {
     "xd"
 }
 
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is alive!")
+
+def run_web_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), SimpleHandler)
+    server.serve_forever()
+
+web_thread = Thread(target=run_web_server, daemon=True)
+web_thread.start()
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
@@ -63,7 +79,7 @@ async def filter_messages(message: types.Message):
 
 async def main():
     logging.basicConfig(level=logging.INFO)
-    print("Бот запущен и фильтрует пачку слов...")
+    print("Бот и веб-сервер запущены...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
